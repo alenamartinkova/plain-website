d10a344390742a8b81046a54469c1d92b695e1ef
Alena Martinkova <martinkova.a@gmail.com>
2020-10-31 18:51:27 +0100

    tabs widget

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -11,3 +11,24 @@ function initMap() {
         map: map,
     });
 }
+
+function changeTab(evt, widgetSelected) {
+    // Declare all variables
+    var i, tabcontent, tabbtns;
+
+    // Get all elements with class="tab-content" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tab-btn" and remove the class "active"
+    tabbtns = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tabbtns.length; i++) {
+        tabbtns[i].className = tabbtns[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(widgetSelected).style.display = "block";
+    evt.currentTarget.className += " active";
+}
